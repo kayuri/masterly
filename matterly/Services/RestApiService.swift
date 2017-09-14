c2ef7bfa70378eca71d564928a094399d4ec0212
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2017-09-14 14:29:56 +0300

    Add support for Popular and Highest Rated This Year sections
    - Add view controllers
    - Add fetch functions

--- matterly/Services/RestApiService.swift
+++ matterly/Services/RestApiService.swift
@@ -7,14 +7,13 @@ class RestApiService {
     fileprivate init() {
     }
     
-    func fetchNewInTheatres(completion: @escaping ([FeedItem]?, NSError?) -> Void ) {
-        
-        let newInTheatresEndpoint: String = "https://api.themoviedb.org/3/discover/movie?certification_country=US&page=1&primary_release_date.gte=2017-09-11&primary_release_date.lte=2017-09-16&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=84d0e365fa21f123b2aca3c2f16bbc54"
+    
+    func fetch(_ endpoint: String, _ completion: @escaping ([FeedItem]?, NSError?) -> Void ) {
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
         
-        guard let url = URL(string: newInTheatresEndpoint) else {
-            print("Error: cannot create URL")
+        guard let url = URL(string: endpoint) else {
+            completion(.none, NSError.message("Error: cannot create URL", code: 0))
             return
         }
         let urlRequest = URLRequest(url: url)
@@ -48,5 +47,20 @@ class RestApiService {
             }
         }
         task.resume()
+    }
+    
+    func fetchHighestRatedThisYear(completion: @escaping ([FeedItem]?, NSError?) -> Void ) {
+        let highestRatedThisYearEndpoint = "https://api.themoviedb.org/3/discover/movie?certification_country=US&vote_count.gte=500&page=1&include_video=false&include_adult=false&sort_by=vote_average.desc&year=2017&language=en-US&api_key=84d0e365fa21f123b2aca3c2f16bbc54"
+        fetch(highestRatedThisYearEndpoint, completion)
+    }
+    
+    func fetchPopular(completion: @escaping ([FeedItem]?, NSError?) -> Void ) {
+        let popularEndpoint = "https://api.themoviedb.org/3/discover/movie?certification_country=US&page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=84d0e365fa21f123b2aca3c2f16bbc54"
+        fetch(popularEndpoint, completion)
+    }
+    
+    func fetchNewInTheatres(completion: @escaping ([FeedItem]?, NSError?) -> Void ) {
+        let newInTheatresEndpoint: String = "https://api.themoviedb.org/3/discover/movie?certification_country=US&page=1&primary_release_date.gte=2017-09-11&primary_release_date.lte=2017-09-16&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=84d0e365fa21f123b2aca3c2f16bbc54"
+        fetch(newInTheatresEndpoint, completion)
     }
 }
